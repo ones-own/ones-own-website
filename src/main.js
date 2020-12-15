87fc9deb8b156bef67486f76bcf8a8691bfc20ca
jeddy3 <jeddy3@users.noreply.github.com>
2020-12-15 16:11:00 +0000

    Add roots section

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("colorschemechange", (e) => {
   if (e.detail.colorScheme === "dark") {
     const toggle = document.getElementById("dark-mode-toggle");
-    const info = document.createElement("div");
+    const info = document.createElement("p");
     info.id = "dark-mode-info";
     info.innerHTML = "You’re now using 60% less energy to view this website";
     toggle.after(info);
